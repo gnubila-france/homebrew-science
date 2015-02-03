1d10df600c31eeda83b49e89621d7d83a6b4cb7e
Jerome Revillard <jrevillard@maatg.fr>
2015-02-03 10:54:53 +0100

    Niftilib: depends on cmake if not on Mac

--- niftilib.rb
+++ niftilib.rb
@@ -5,13 +5,15 @@ class Niftilib < Formula
   url 'https://downloads.sourceforge.net/project/niftilib/nifticlib/nifticlib_2_0_0/nifticlib-2.0.0.tar.gz'
   sha1 '3a6187cb09767f97cef997cf492d89ac3db211df'
 
+  depends_on 'cmake' => :build unless OS.mac?
+
   def install
     ENV.deparallelize
     if OS.mac?
-    system "make"
-    bin.install Dir['bin/*']
-    lib.install Dir['lib/*']
-    include.install Dir['include/*']
+      system "make"
+      bin.install Dir['bin/*']
+      lib.install Dir['lib/*']
+      include.install Dir['include/*']
     else 
       system "cmake -Wno-dev -G'Unix Makefiles' -DCMAKE_SKIP_RPATH=ON -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=on -DCMAKE_INSTALL_PREFIX=#{prefix}"
       system "make"
